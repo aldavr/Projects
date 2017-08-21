02007a6d31453fc5406f9a7137c86c239bb70a1c
Davron Aliyev <davronaliyev@gmail.com>
2017-08-21 11:08:59 -0400

    first commit

---
+++ water-adventure-app/Framework/Framework/ext-modules/psMenu/psMenuController.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+angular.module('psMenu').controller('psMenuController',
+    ['$scope',
+        function ($scope) {
+            this.setActiveElement = function(el) {
+                $scope.activeElement = el;
+            };
+            this.setRoute = function(route) {
+                $rootScope.$broadcast('ps-menu-item-selected-event',
+                { route: route });
+            };
+        }
+    ]);
